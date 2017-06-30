ae2ac026eb7c2fe38499a7c5ac4eba632261c66a
amrithadevi <sangeethameganathan98@gmail.com>
2017-06-30 11:02:46 +0530

    Create ddd.c

---
+++ ddd.c
@@ -0,0 +1,11 @@
+void main()
+{
+char a;
+printf("enter the character");
+scanf("%c",&a);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='u')
+printf("vowel");
+else
+printf("consonant");
+getch();
+}
